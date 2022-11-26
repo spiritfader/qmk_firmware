a7bad99aeca2bd74b0f1ee803e6b5624b7e54aaa
Autumn <31056954+spiritfader@users.noreply.github.com>
2022-11-26 00:26:26 -0500

    Initialize and upload my keymap
    Initialize and upload my custom keymap for quefrency rev 3

---
+++ keyboards/keebio/quefrency/keymaps/spiritfader/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define USE_SERIAL 
+#define USB_POLLING_INTERVAL_MS 1
+#define FORCE_NKRO
+#define EE_HANDS
---
+++ keyboards/keebio/quefrency/keymaps/spiritfader/rules.mk
@@ -0,0 +1,4 @@
+VIA_ENABLE = yes		# Enable VIA support
+NKRO_ENABLE = yes		# Enable NKRO functionality
+BACKLIGHT_ENABLE = no   # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes   # Enable WS2812 RGB underlight.
